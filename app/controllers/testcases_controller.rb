ba24334a029f753786942e8156ac0243451b2bd7
StasLakh <doctorbyron05@gmail.com>
2017-01-04 06:20:31 -0800

    add testcases

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -1,2 +1,19 @@
 class TestcasesController < ApplicationController
+
+  def create
+    @casetitle = Casetitle.find(params[:casetitle_id]).testcases.create(testcase_params)
+    redirect_to casetitle_path(@casetitle)
+  end
+
+  def destroy
+    @casetitle = Casetitle.find(params[:casetitle_id]).testcases.find(params[:id]).destroy
+    redirect_to casetitle_path(@casetitle)
+  end
+
+  private
+
+  def testcase_params
+    params.require(:testcase).permit(:texpresult, :envir, :tester)
+  end
+
 end
